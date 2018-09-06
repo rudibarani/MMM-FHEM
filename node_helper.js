6269d6147e2936b340663269e7b75780b4840abc
rudibarani <7797301+rudibarani@users.noreply.github.com>
2018-09-06 14:02:00 +0200

    Update node_helper.js

--- node_helper.js
+++ node_helper.js
@@ -65,7 +65,7 @@ module.exports = NodeHelper.create({
       var FHEMvalue = parseFloat(device.Readings[readingName].Value);
 
       if (FHEMvalue) {
-        FHEMvalue.toFixed(0);
+        FHEMvalue.toFixed(2);
         values.push(FHEMvalue);
       } else {
         values.push('Reading not exist');
